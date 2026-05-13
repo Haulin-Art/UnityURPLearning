eec682d1a1cbe1683fa28e6abbd376c1ea8904ca
Lynn <2797096902@qq.com>
2026-05-13 22:48:45 +0800

    弹簧质点模型完成，比较粗糙的布料破洞模拟

--- Assets/m_Effect/DynamicPantyhoseTearing/DynamicPantyhoseTearing.cs
+++ Assets/m_Effect/DynamicPantyhoseTearing/DynamicPantyhoseTearing.cs
@@ -1,3 +1,4 @@
+using Unity.Profiling;
 using UnityEngine;
 
 /// <summary>
@@ -66,11 +67,33 @@ public class DynamicPantyhoseTearingNS : MonoBehaviour
     private RenderTexture dispRead;
     private RenderTexture dispWrite;
 
+
+    /// ********* 粒子数据信息 *********************** 
+    /// 粒子位置信息，xy是当前的位置，zw是上一帧的位置
+    private RenderTexture particlePosRead;    
+    private RenderTexture particlePosWrite;
+    // 粒子状态信息，x是记录当前粒子是否激活
+    private RenderTexture particleStateRead;
+    private RenderTexture particleStateWrite;
+    private int initParticleKernel;    // 用于将粒子位置纹理初始化为uv
+    private int updateParticleKernel;  // 用于更新粒子位置和状态
+    private float particleGridSize = 60.0f;
+
+
     // 射线检测缓存
     private bool isHit = false;
     private Vector2 hitUV = Vector2.zero;
     private Vector2 previousHitUV = Vector2.zero;
     private bool previousIsHit = false;
+
+
+
+
+    // 性能检测
+    // 静态化避免重复构造，内部使用完整命名空间路径方便在Profiler中折叠分类
+    static readonly ProfilerMarker s_MyUpdateLogic = new ProfilerMarker("MySystem.UpdateLogic");
+
+
 
     private void Start()
     {
@@ -88,27 +111,49 @@ public class DynamicPantyhoseTearingNS : MonoBehaviour
 
         initKernel = computeShader.FindKernel("InitKernel");
         tearingKernel = computeShader.FindKernel("TearingKernel");
+
+        /// ********* 粒子数据信息 *********************** 
+        /// 初始化粒子位置（仅运行一次）
+        initParticleKernel = computeShader.FindKernel("InitParticleKernel");
+        updateParticleKernel = computeShader.FindKernel("UpdateParticleKernel");
+
         CreateRenderTextures();
 
         // 初始化位移场（仅运行一次）
         InitDispField();
+
+        // 初始化粒子位置（仅运行一次）
+        InitParticlePos();
     }
 
     private void Update()
     {
         if (computeShader == null) return;
 
+        
+        s_MyUpdateLogic.Begin();
+
         UpdateRaycastData();
         Simulate();
+
+        /// 更新粒子位置
+        UpdateParticlePos();
+
+
 
         if (outputTexture != null)
             Graphics.Blit(holeRead, outputTexture);
 
         if (displayMaterial != null)
         {
-            displayMaterial.SetTexture("_HoleTex", holeRead);
+            displayMaterial.SetTexture("_Particle", holeRead);
             displayMaterial.SetTexture("_DispTex", dispRead);
+            displayMaterial.SetTexture("_ParticlePos", particlePosRead);
+            displayMaterial.SetTexture("_ParticleState", particleStateRead);
+            displayMaterial.SetTexture("_MinDisPos", dispRead);
         }
+
+        s_MyUpdateLogic.End();
     }
 
     private void OnDestroy()
@@ -150,6 +195,28 @@ public class DynamicPantyhoseTearingNS : MonoBehaviour
         dispRead.Create();
         dispWrite = new RenderTexture(dispDesc) { name = "DispWrite" };
         dispWrite.Create();
+
+
+        /// ********* 粒子数据信息 *********************** 
+        /// 粒子位置信息，xy是当前的位置，zw是上一帧的位置
+        /// 大小设定为最终纹理的1/16，以减少内存占用和计算量
+        var particlePosDesc = new RenderTextureDescriptor((int)particleGridSize, (int)particleGridSize, RenderTextureFormat.ARGBFloat, 0)
+        {
+            enableRandomWrite = true
+        };
+        particlePosRead = new RenderTexture(particlePosDesc) { name = "ParticlePosRead" };
+        particlePosRead.Create();
+        particlePosWrite = new RenderTexture(particlePosDesc) { name = "ParticlePosWrite" };
+        particlePosWrite.Create();
+        
+        var particleStateDesc = new RenderTextureDescriptor((int)particleGridSize, (int)particleGridSize, RenderTextureFormat.RFloat, 0)
+        {
+            enableRandomWrite = true
+        };
+        particleStateRead = new RenderTexture(particleStateDesc) { name = "ParticleStateRead" };
+        particleStateRead.Create();
+        particleStateWrite = new RenderTexture(particleStateDesc) { name = "ParticleStateWrite" };
+        particleStateWrite.Create();
     }
 
     private void ReleaseRenderTextures()
@@ -158,6 +225,11 @@ public class DynamicPantyhoseTearingNS : MonoBehaviour
         if (holeWrite != null) { holeWrite.Release(); Destroy(holeWrite); }
         if (dispRead != null)  { dispRead.Release(); Destroy(dispRead); }
         if (dispWrite != null) { dispWrite.Release(); Destroy(dispWrite); }
+
+        /// ********* 粒子数据信息 *********************** 
+        /// 粒子位置信息，xy是当前的位置，zw是上一帧的位置
+        if (particlePosRead != null)  { particlePosRead.Release(); Destroy(particlePosRead); }
+        if (particlePosWrite != null) { particlePosWrite.Release(); Destroy(particlePosWrite); }
     }
 
     private void InitDispField()
@@ -165,6 +237,7 @@ public class DynamicPantyhoseTearingNS : MonoBehaviour
         int threadGroups = Mathf.CeilToInt(size / 8.0f);
 
         computeShader.SetFloat("_TexSize", size);
+        computeShader.SetFloat("_ParticleGridSize", particleGridSize);
         computeShader.SetTexture(initKernel, "_DispWrite", dispWrite);
         computeShader.SetTexture(initKernel, "_HoleWrite", holeWrite);
         computeShader.Dispatch(initKernel, threadGroups, threadGroups, 1);
@@ -174,12 +247,62 @@ public class DynamicPantyhoseTearingNS : MonoBehaviour
         Swap(ref holeRead, ref holeWrite);
     }
 
+    private void InitParticlePos()
+    {
+        int threadGroups = Mathf.CeilToInt((int)particleGridSize / 8.0f);
+
+        computeShader.SetFloat("_ParticleGridSize", particleGridSize);
+        computeShader.SetTexture(initParticleKernel, "_ParticlePosWrite", particlePosWrite);
+        computeShader.SetTexture(initParticleKernel, "_ParticleStateWrite", particleStateWrite);
+        computeShader.Dispatch(initParticleKernel, threadGroups, threadGroups, 1);
+
+        // Swap 后将初始化数据放入读缓冲
+        //Graphics.Blit(particlePosWrite, particlePosRead);
+        Swap(ref particlePosRead, ref particlePosWrite);
+        //Graphics.Blit(particleStateWrite, particleStateRead);
+        Swap(ref particleStateRead, ref particleStateWrite);
+    }
+    private void UpdateParticlePos()
+    {
+        int threadGroups = Mathf.CeilToInt((int)particleGridSize / 8.0f);
+
+        // 画笔输入
+        bool pressed = Input.GetMouseButton(0) && isHit;
+        computeShader.SetFloat("_KeyDown", pressed ? 1.0f : 0.0f);
+
+        Vector2 curUV = isHit ? hitUV : Vector2.one * -1.0f;
+        Vector2 prevUV = previousIsHit ? previousHitUV : curUV + Vector2.one * 1e-4f;
+        computeShader.SetVector("_PenPos", new Vector4(curUV.x, curUV.y, prevUV.x, prevUV.y));
+
+        computeShader.SetFloat("_Radius", penRadius);
+
+        computeShader.SetFloat("_ParticleGridSize", particleGridSize);
+        computeShader.SetFloat("_DT", dt);
+        computeShader.SetTexture(updateParticleKernel, "_ParticlePosRead", particlePosRead);
+        computeShader.SetTexture(updateParticleKernel, "_ParticlePosWrite", particlePosWrite);
+        computeShader.SetTexture(updateParticleKernel, "_ParticleStateRead", particleStateRead);
+        computeShader.SetTexture(updateParticleKernel, "_ParticleStateWrite", particleStateWrite);
+        
+        computeShader.Dispatch(updateParticleKernel, threadGroups, threadGroups, 1);
+
+        // Swap 后将更新数据放入读缓冲
+        //Graphics.Blit(particlePosWrite, particlePosRead);
+        Swap(ref particlePosRead, ref particlePosWrite);
+        //Graphics.Blit(particleStateWrite, particleStateRead);
+        //Swap(ref particleStateRead, ref particleStateWrite);
+        
+    }
+
+
     private void Simulate()
     {
         int threadGroups = Mathf.CeilToInt(size / 8.0f);
 
         // 共享参数
         computeShader.SetFloat("_TexSize", size);
+
+        computeShader.SetFloat("_ParticleGridSize", particleGridSize);
+
         computeShader.SetFloat("_DT", dt);
         computeShader.SetFloat("_Radius", penRadius);
         computeShader.SetFloat("_RetractionStrength", retractionStrength);
@@ -198,6 +321,13 @@ public class DynamicPantyhoseTearingNS : MonoBehaviour
         computeShader.SetTexture(tearingKernel, "_HoleTex", holeRead);
         computeShader.SetTexture(tearingKernel, "_DispTex", dispRead);
 
+        // 绑定粒子读缓冲
+        computeShader.SetTexture(tearingKernel, "_ParticlePosRead", particlePosRead);
+        computeShader.SetTexture(tearingKernel, "_ParticleStateRead", particleStateRead);
+        // 绑定粒子状态写缓冲
+        computeShader.SetTexture(tearingKernel, "_ParticleStateWrite", particleStateWrite);
+        //computeShader.SetTexture(tearingKernel, "_ParticleStateRead", particleStateRead);
+
         // 绑定可写缓冲
         computeShader.SetTexture(tearingKernel, "_HoleWrite", holeWrite);
         computeShader.SetTexture(tearingKernel, "_DispWrite", dispWrite);
@@ -208,6 +338,8 @@ public class DynamicPantyhoseTearingNS : MonoBehaviour
         // Ping-pong swap
         Swap(ref holeRead, ref holeWrite);
         Swap(ref dispRead, ref dispWrite);
+        Swap(ref particleStateRead, ref particleStateWrite);
+
     }
 
     private static void Swap<T>(ref T a, ref T b)
